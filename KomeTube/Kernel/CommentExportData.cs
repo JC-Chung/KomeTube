a8bb1195bf792dcf1e347da8c335f156eb86c945
JC-Chung <52159296+JC-Chung@users.noreply.github.com>
2022-10-11 14:04:30 +0800

    Update
    Add export data

--- KomeTube/Kernel/CommentExportData.cs
+++ KomeTube/Kernel/CommentExportData.cs
@@ -14,5 +14,9 @@ namespace KomeTube.Kernel
         public string Message { get; set; }
         public string PaidMsg { get; set; }
         public string AuthorID { get; set; }
+        public string Membership { get; set; }
+        public string GiftRedemption { get; set; }
+        public string GiftPurchase { get; set; }
+        public string PaidStk { get; set; }
     }
 }
